da106608992fa9091a3d516c0f53d45cac7e4422
Farid Joubbi <djfarid@gmail.com>
2016-04-18 14:44:20 +0000

    Upload of the new file

---
+++ check_ohm_temperatures.ps1
@@ -0,0 +1,73 @@
+<#
+.SYNOPSIS
+This is a PowerShell script used by NSClient++ to check the temperatures on a host running Windows.
+NSClient++ can then be called by Nagios (Op5 Monitor, Icinga or similar) to run this script.
+
+.DESCRIPTION
+Open Hardware Monitor (openhardwaremonitor.org) is used as a driver for the temperature sensors.
+Open Hardware monitor creates WMI objects of all the found sensors.
+This script retrieves the temperatures from those WMI objects.
+This means that you have to download and run OpenHardwareMonitor.exe before running this check.
+
+All the found temperatures will be output as performance data so that they can be graphed.
+
+Define the command in nsclient++:
+cmd /c echo scripts\custom\check_temperatures.ps1 -warning $ARG1$ -critical $ARG2$; exit($lastexitcode) | powershell.exe -command -
+or you can omit or hard code the warning and critical arguments in case you do not permit sending arguments to nsclient.
+
+.EXAMPLE
+.\check_temperatures.ps1 -warning 80 -critical 90
+
+.NOTES
+Licensed under the Apache license.
+Written by farid.joubbi@consign.se
+
+1.1 2016-03-24 Minor cleanup of variables and documentation.
+1.0 2016-03-11 Initial release.
+
+
+.LINK
+http://consign.se/
+http://nsclient.org/
+http://openhardwaremonitor.org/
+
+#>
+
+param (
+    [ValidateRange(0,200)]
+    [Int]
+    [string]$warning = 70,
+    [ValidateRange(0,200)]
+    [Int]
+    [string]$critical = 80
+)
+
+$status='OK'
+
+
+# Check if Open Hardware Monitor is running.
+if ((Get-Process -Name OpenHardwareMonitor -ErrorAction SilentlyContinue) -eq $null) {
+    write-host 'OpenHardwareMonitor.exe not running!'
+    exit 3
+}
+
+# Check that critical is set to higher than warning
+if ($warning -gt $critical) {
+    write-host 'warning set to higher than critical temperature!'
+    exit 3
+}
+
+# Get the temperatures from all the found sensors and check them
+$temperatures = Get-WmiObject -Namespace "Root\OpenHardwareMonitor" -Query "SELECT * FROM Sensor WHERE Sensortype='Temperature'" | sort-object Identifier
+
+$temperature_string = foreach ($result in $temperatures){
+    $result.Identifier = $result.Identifier -replace '^/','' -replace 'temperature/',''
+    '{0}={1};{2};{3};{4};{5}' -f $result.Identifier, $result.Value, $warning, $critical, $result.Min, $result.Max
+    if ($result.value -gt $warning) { $status = 'WARNING'}
+    if ($result.value -gt $critical) { $status = 'CRITICAL'}
+}
+ 
+write-host "temperatures $status | $temperature_string"
+if ($status -eq 'WARNING') { exit 1 }
+if ($status -eq 'CRITICAL') { exit 2 }
+exit 0
